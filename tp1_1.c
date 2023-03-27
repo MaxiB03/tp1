d552f20f219bb5bfc13a9481087eb2f92173c78b
batallan <batallanmaximo03@gmail.com>
2023-03-26 22:06:05 -0300

    archivo c modificado

--- tp1_1.c
+++ tp1_1.c
@@ -4,5 +4,18 @@ int main() {
 
     printf("Hola mundo");
 
+    int *p, entero=5;
+
+    p = &entero;
+
+    //Mostrar el contenido del puntero
+    printf("\nContenido del puntero: %d\n", *p);
+
+    //Mostrar la direccion de memoria de la variable
+    printf("Direccion de memoria de la variable: %d\n", &entero);
+
+    //Mostrar la direccion de memoria del puntero
+    printf("Direccion de memoria del puntero: %d\n", &p);
+
     return 0;
 }
